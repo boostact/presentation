cc03bb00fff08bbb97c4f9e3c9df024945824491
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-12-15 16:26:31 +0900

    [chore] file 전체 설정 #0

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import { Boostact } from "boostact";
+/** @jsx Boostact.createElement */
+const App = () => {
+    return (
+        <div>
+            <h1>Hello!</h1>
+            <h2>This is Boostact!</h2>
+        </div>
+    );
+};
+export default App;
---
+++ index.js
@@ -0,0 +1,6 @@
+import { Boostact } from "boostact";
+import App from "./App";
+/** @jsx Boostact.createElement */
+
+const root = document.getElementById("root");
+Boostact.render(<App />, root);
