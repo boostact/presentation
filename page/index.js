c22a9f02bb3f80d2fb6fd3a44e1fbbf80d185d5a
ji <ji_meng@naver.com>
2020-12-16 21:46:58 +0900

    page 12 초안

--- page/index.js
+++ page/index.js
@@ -10,7 +10,7 @@ import Page_8 from "./Page_8";
 import Page_9 from "./Page_9";
 import Page_10 from "./Page_10";
 import Page_11 from "./Page_11";
-import Page_12 from "./Page_12";
+import Page_12 from "./page_12/Page_12";
 import Page_13 from "./Page_13";
 import Page_14 from "./page_14/Page_14";
 
