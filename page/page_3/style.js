941af8934f7a2084cc2eb2cb0d5e3c4584d23066
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-12-16 16:51:10 +0900

    fix : page_3 충돌 해소 및 라우팅 동작 확인

--- page/page_3/style.js
+++ page/page_3/style.js
@@ -1,37 +1,27 @@
-import jss from 'jss';
-import preset from 'jss-preset-default';
+import jss from "jss";
+import preset from "jss-preset-default";
 
 jss.setup(preset());
 
 const style = {
-  radious: `
-        width:300px;
-        height:300px;
-        line-height: 20px;
-        text-align:center;
-        vertical-align:middle;
-        padding:10px;
-        border-radius: 50%;
-        color: white;
-    `,
-  radiousText: `
-        margin-top:45%;
-        font-size:35px;
-    `,
-  radiousContainer: `
-        margin-top:240px;
-        display:flex;
-        justify-content: space-around;
-    `,
-  red: `
-        background:red;
-    `,
-  blue: `
-        background:blue;
-    `,
-  green: `
-        background:green;
-    `,
+    page3: {
+        minHeight: "100%",
+        width: "100%",
+    },
+    imageBox: {
+        textAlign: "center",
+        height: "100%",
+        paddingTop: "0px",
+    },
+    treeImage: {
+        width: "50%",
+        bottom: "0px",
+    },
+    framework: {
+        position: "absoulte",
+        color: "white",
+        top: "50%",
+    },
 };
 
 const { classes } = jss.createStyleSheet(style).attach();
