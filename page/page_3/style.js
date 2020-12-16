ddf3c8cda8db53f1afceb1a6b03de00c8bef263a
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-12-16 16:22:24 +0900

    fix : style page_3 충돌 해소

--- page/page_3/style.js
+++ page/page_3/style.js
@@ -4,7 +4,6 @@ import preset from "jss-preset-default";
 jss.setup(preset());
 
 const style = {
-<<<<<<< HEAD
     page3: {
         minHeight: "100%",
         width: "100%",
@@ -23,36 +22,6 @@ const style = {
         color: "white",
         top: "50%",
     },
-=======
-    radious : `
-        width:300px;
-        height:300px;
-        line-height: 20px;
-        text-align:center;
-        vertical-align:middle;
-        padding:10px;
-        border-radius: 50%;
-        color: white;
-    `,
-    radiousText:`
-        margin-top:45%;
-        font-size:35px;
-    `,
-    radiousContainer : `
-        margin-top:240px;
-        display:flex;
-        justify-content: space-around;
-    `,
-    red:`
-        background:red;
-    `,
-    blue:`
-        background:blue;
-    `,
-    green:`
-        background:green;
-    `
->>>>>>> 89317492bf9ea0d172c5051a617a9a74c4986044
 };
 
 const { classes } = jss.createStyleSheet(style).attach();
