fe83654f8d7bf46447fb7dcabf0b4b31144afad5
ji <ji_meng@naver.com>
2020-12-17 14:54:11 +0900

    [feat] 코드박스 hover 확대 기능 추가

--- page/page_8/style.js
+++ page/page_8/style.js
@@ -1,15 +1,22 @@
 import jss from "jss";
 import preset from "jss-preset-default";
+import animation from "../common/animation";
 
 jss.setup(preset());
 
 const style = {
+  "@keyframes expanse": animation.expanse,
     entireBox: {
         display: "flex",
         paddingLeft: "50px",
+        justifyContent:"center"
     },
 
     codeBox: {
+        '&:hover':{
+            animationName: "$expanse",
+            animationDuration: "7s",
+        },
         width: "100%",
         backgroundColor: "#1E1E1E",
         color: "white",
